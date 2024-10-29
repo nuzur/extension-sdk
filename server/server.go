3647dc4bc6c6a057dcfeb129f30753e6bf1d6b75
Maykel Farha <1123614+mklfarha@users.noreply.github.com>
2024-10-28 18:54:39 -0600

    propagate metadata

--- server/server.go
+++ server/server.go
@@ -12,6 +12,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -24,7 +25,13 @@ type Params struct {
 
 func New(params Params) *grpc.Server {
 	log := params.Logger
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+			md, _ := metadata.FromIncomingContext(ctx)
+			ctx = metadata.NewOutgoingContext(ctx, md)
+			return handler(ctx, req)
+		}),
+	)
 	params.Lifecycle.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			// GRPC port from config
