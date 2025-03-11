299b99cc26404c6776781a7d17fb7fbec5d462c9
Maykel Farha <1123614+mklfarha@users.noreply.github.com>
2025-03-10 21:51:19 -0600

    localize refactor to avoid panic

--- client/localize.go
+++ client/localize.go
@@ -1,23 +1,31 @@
 package client
 
 import (
+	"fmt"
+
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 )
 
 func (c *Client) Localize(key string, lang string, defaultValue string) string {
 	localizer := i18n.NewLocalizer(c.i18nbundle, lang)
-	return localizer.MustLocalize(&i18n.LocalizeConfig{
+	localized, err := localizer.Localize(&i18n.LocalizeConfig{
 		MessageID: key,
 		DefaultMessage: &i18n.Message{
 			ID:    key,
 			Other: defaultValue,
 		},
 	})
+
+	if err != nil {
+		fmt.Printf("Error localizing key %s: %s\n", key, err)
+		return defaultValue
+	}
+	return localized
 }
 
 func (c *Client) LocalizeWithVariables(key string, lang string, variables map[string]string, defaultValue string) string {
 	localizer := i18n.NewLocalizer(c.i18nbundle, lang)
-	return localizer.MustLocalize(&i18n.LocalizeConfig{
+	localized, err := localizer.Localize(&i18n.LocalizeConfig{
 		MessageID:    key,
 		TemplateData: variables,
 		DefaultMessage: &i18n.Message{
@@ -25,4 +33,10 @@ func (c *Client) LocalizeWithVariables(key string, lang string, variables map[st
 			Other: defaultValue,
 		},
 	})
+
+	if err != nil {
+		fmt.Printf("Error localizing key %s: %s\n", key, err)
+		return defaultValue
+	}
+	return localized
 }
