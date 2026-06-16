835cf11a8c7d8aa33ac2499f02899635690213ad
Maykel Farha <1123614+mklfarha@users.noreply.github.com>
2026-06-16 13:14:02 -0600

    remove StopTimeout from module + use latest nem

--- module.go
+++ module.go
@@ -1,8 +1,6 @@
 package extensionsdk
 
 import (
-	"time"
-
 	"github.com/nuzur/extension-sdk/client"
 	"github.com/nuzur/extension-sdk/server"
 	"go.uber.org/fx"
@@ -10,7 +8,6 @@ import (
 )
 
 var Module = fx.Module("extensionsdk",
-	fx.StopTimeout(45 * time.Second),
 	fx.Provide(
 		zap.NewProduction,
 		client.New,
